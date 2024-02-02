d196d74ac6ee9868050590741081f06359cc6aef
julio4ai <juliocolomer@gmail.com>
2024-02-02 10:13:32 +0100

    456

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import chromadb
+import sqlite3
 from langchain_openai import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.embeddings.openai import OpenAIEmbeddings
